03eb714f4968d042621280d92a7d79b5abe537dc
Fangal-Airbag <77993079+Fangal-Airbag@users.noreply.github.com>
2021-08-04 21:05:42 -0400

    Add files via upload

---
+++ wiiu/source/player.h
@@ -0,0 +1,35 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <vector>
+
+typedef struct {
+    int x;
+    int y;
+} Vector2;
+
+class Player {
+private:
+    double x;
+    double y;
+    int width;
+    int height;
+    bool onPlatform;
+    Vector2 velocity;
+    
+public:
+    Player(double x, double y, int width, int height);
+    double getX();
+    double getY();
+    void setX(int x);
+    void setY(int y);
+    int getWidth();
+    int getHeight();
+    bool isOnGround();
+    bool isOnPlatform();
+    void setOnPlatform(bool result);
+    void setVelocity(double x, double y);
+    Vector2 getVelocity();
+    void updatePosition();
+};
+#endif
