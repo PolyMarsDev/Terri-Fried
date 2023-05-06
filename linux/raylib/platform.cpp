7618946759888ab7cf1d39f5a724876efcfc3035
VoxelTek <53562267+VoxelTek@users.noreply.github.com>
2023-05-06 13:50:49 +1000

    Moved files and folders around, added Raylib Linux
    Relocated files and folders, so that the Linux directory more closely matches that of the Windows directory.
    
    Additionally modified the source code for the Raylib version to allow it to compile on Linux, which it fails to in a way that I'd also expect to happen on Windows, but I can't test that.
    
    At the very least, the code has now been fixed to compile and function on Linux.

---
+++ linux/raylib/platform.cpp
@@ -0,0 +1,78 @@
+#include "raylib.h"
+#include "platform.h"
+#include <math.h>
+const int screenWidth = 800;
+const int screenHeight = 450;
+Platform::Platform(int index)
+{
+    width = 100;
+    height = 32;
+    x = rand()% 660 + 20;
+    y = 0 - height - (index * 100);
+    int coinInt = rand()% 4;
+    if (coinInt == 0 || index == 0)
+    {
+        hasCoin = false;
+    } else {
+        hasCoin = true;
+    }
+    coinX = x + width/2 - 24/2;
+    coinY = y - 24 - 5;
+    
+}
+
+double Platform::getX()
+{
+    return x;
+}
+
+double Platform::getY()
+{
+    return y;
+}
+
+int Platform::getWidth()
+{
+    return width;
+}
+
+int Platform::getHeight()
+{
+    return height;
+}
+
+bool Platform::getHasCoin()
+{
+    return hasCoin;
+}
+void Platform::setHasCoin(bool value)
+{
+    hasCoin = value;
+}
+int Platform::getCoinX()
+{
+    return coinX;
+}
+int Platform::getCoinY()
+{
+    return coinY;
+}
+
+void Platform::updatePosition()
+{
+    y+=1;
+    coinX = x + width/2 - 24/2;
+    coinY = y - 24 - 5;
+    if (y > screenHeight)
+    {
+        x = rand()% 660 + 20;
+        y = 0 - height;
+        int coinInt = rand()% 4;
+        if (coinInt == 0)
+        {
+            hasCoin = false;
+        } else {
+            hasCoin = true;
+        }
+    }
+}
